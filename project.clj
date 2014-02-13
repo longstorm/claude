f91e9fc53a2917c1802d606be7b2fa015917de28
Vic Goldfeld <vic@longstorm.org>
2014-02-13 01:35:06 -0200

    create project from lein new

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject claude "0.1.0"
+  :description "Clojure meets node: the essentials wrapped in parens."
+  :url "https://github.com/goldfeld/claude"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/goldfeld/claude.git"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
