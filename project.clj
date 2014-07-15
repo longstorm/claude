05f3497a98fbaa385395b6f85b86f21afe83651e
Vic Goldfeld <vic@longstorm.org>
2014-07-15 07:23:54 -0300

    add build and source-path directives

--- project.clj
+++ project.clj
@@ -6,7 +6,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/longstorm/claude.git"}
+  :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2202"]
                  [org.bodil/redlobster "0.2.1"]]
-  :profiles {:dev {:dependencies [[org.bodil/cljs-noderepl "0.1.10"]]}})
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:target :nodejs
+                                   :output-to "target/claude.js"
+                                   :optimizations :simple}}]})
