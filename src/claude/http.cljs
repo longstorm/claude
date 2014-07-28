13df30eea8835c75fb0cca8a82bde14cfce8ca8b
Vic Goldfeld <vic@longstorm.org>
2014-07-28 13:39:05 -0300

    add callback version to http/https 'get' fn

--- src/claude/http.cljs
+++ src/claude/http.cljs
@@ -1,11 +1,11 @@
 (ns claude.http
   (:refer-clojure :exclude [get])
   (:require-macros [redlobster.macros :refer [promise]])
-  (:require [cljs.nodejs :as node]))
+  (:require [cljs.nodejs :as node]
+            [cljs.core.async :as a]))
 
 (def ^:private Http (memoize #(node/require "http")))
 
-(defn get [url]
-  (promise (.get (Http) url (fn [res]
-                              (.log js/console (.-statusCode res))
-                              (realise res)))))
+(defn get
+  ([url] (promise (.get (Http) url (fn [res] (realise res)))))
+  ([url callback] (.get (Http) url callback)))
--- src/claude/https.cljs
+++ src/claude/https.cljs
@@ -1,11 +1,11 @@
 (ns claude.https
   (:refer-clojure :exclude [get])
   (:require-macros [redlobster.macros :refer [promise]])
-  (:require [cljs.nodejs :as node]))
+  (:require [cljs.nodejs :as node]
+            [cljs.core.async :as a]))
 
 (def ^:private Https (memoize #(node/require "https")))
 
-(defn get [url]
-  (promise (.get (Https) url (fn [res]
-                               (.log js/console (.-statusCode res))
-                               (realise res)))))
+(defn get
+  ([url] (promise (.get (Https) url (fn [res] (realise res)))))
+  ([url callback] (.get (Https) url callback)))
