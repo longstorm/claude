0e567c9d7df2b0c9e83bd6021d15d57b3e6acf9d
Vic Goldfeld <vic@longstorm.org>
2014-11-28 20:51:58 -0200

    swap core.async for promises in child_process

--- src/claude/child_process.cljs
+++ src/claude/child_process.cljs
@@ -1,21 +1,30 @@
 (ns claude.child-process
-  (:require [cljs.nodejs :as node]))
+  (:require [cljs.nodejs :as node]
+            [cljs.core.async :as a]))
 
 (def ^:private Child_process (memoize #(node/require "child_process")))
 
 (def ^:private get-exec
   (memoize (fn [] (.-exec (Child_process)))))
 
-(defn exec
-  ([cmd] ((get-exec) cmd))
-  ([cmd cb] (get-exec) cmd (clj->js {}) cb))
+(defn exec [cmd]
+  (let [chan (a/chan)]
+    ((get-exec-file) cmd (clj->js {}) (fn [err res] (a/put! chan res)))
+    chan))
 
 (def ^:private get-exec-file
   (memoize (fn [] (.-execFile (Child_process)))))
 
 (defn exec-file
-  ([cmd args cb] ((get-exec-file) cmd (clj->js args) cb))
-  ([cmd args opts cb] ((get-exec-file) cmd (clj->js args) (clj->js opts) cb)))
+  ([cmd args]
+     (let [chan (a/chan)]
+       ((get-exec-file) cmd (clj->js args) (fn [err res] (a/put! chan res)))
+       chan))
+  ([cmd args opts]
+     (let [chan (a/chan)]
+       ((get-exec-file) cmd (clj->js args) (clj->js opts)
+        (fn [err res] (a/put! chan res)))
+       chan)))
 
 (def ^:private get-spawn
   (memoize (fn [] (.-spawn (Child_process)))))
